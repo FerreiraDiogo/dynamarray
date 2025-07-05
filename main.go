32a3ca5f7fec3479dbf43ac268ab04da3bc28f0e
Diogo Ferreira <diogobeen@gmail.com>
2025-07-05 14:35:08 -0300

    added append and list functionalities

--- main.go
+++ main.go
@@ -1,8 +1,64 @@
 package main
 
-import "dynamarray/messages"
+import (
+	"dynamarray/messages"
+	"fmt"
+)
+
+const INVALID_INPUT_CHARACTERS = "+_)(*&¨%$#@!\\/|:;>.<,}]^~{[]´`)"
+
+var dynamarray []int
+
+func init() {
+	dynamarray = make([]int, 0)
+}
 
 func main() {
 
-	messages.PrintWelcomeMessage()
+	running := true
+	var input int
+
+	for running {
+		messages.PrintWelcomeMessage()
+
+		_, inputErr := fmt.Scanf("%d", &input)
+		if inputErr != nil {
+			messages.PrintErrorInputMessage()
+			continue
+		}
+		running = selectOption(input)
+	}
+
+	messages.PrintGoodbyeMessage()
+}
+
+func selectOption(input int) bool {
+	switch input {
+	case 0:
+		return false
+	case 1:
+		appendToSlice()
+		return true
+	case 4:
+		messages.PrettyPrint(dynamarray)
+		return true
+	default:
+		messages.PrintErrorInputMessage()
+		return true
+	}
+}
+
+func appendToSlice() {
+	messages.PrintAppendMessage()
+	var element int
+	running := true
+	for running {
+		_, inputErr := fmt.Scanf("%d", &element)
+		if inputErr != nil {
+			messages.PrintErrorInputMessage()
+			continue
+		}
+		running = false
+	}
+	dynamarray = append(dynamarray, element)
 }
--- messages/messages.go
+++ messages/messages.go
@@ -8,3 +8,23 @@ func PrintWelcomeMessage() {
 	fmt.Print("0 - Quit Program\n1 - Append Integer to Slice\n2 - Prepend Integer to Slice\n3 - Insert Integer into especified position\n" +
 		"4 - List all integers\n5 - Find integer\n6 - Remove integer\n7 - Reorder\n")
 }
+
+func PrintGoodbyeMessage() {
+	fmt.Println("Thanks for using Dynamarray!")
+}
+
+func PrintErrorInputMessage() {
+	fmt.Println("Please, type a valid option")
+}
+
+func PrettyPrint(list []int) {
+	fmt.Print("The elements in the slice are: [ ")
+	for _, v := range list {
+		fmt.Printf("%d, ", v)
+	}
+	fmt.Println("]")
+}
+
+func PrintAppendMessage() {
+	fmt.Println("Insert the value to be appended to the slice")
+}
