d14f04578ee3a09ac086ea99b73bc83336ef47c7
Diogo Ferreira <diogobeen@gmail.com>
2025-07-14 17:57:13 -0300

    implementation of quicksort for ascending and descending sorting

--- messages/messages.go
+++ messages/messages.go
@@ -6,7 +6,7 @@ func PrintWelcomeMessage() {
 	fmt.Println("==========Dynamarray V1.0.0==========")
 	fmt.Println("==========     Options     ==========")
 	fmt.Print("0 - Quit Program\n1 - Append Integer to Slice\n2 - Prepend Integer to Slice\n3 - Insert Integer into especified position\n" +
-		"4 - List all integers\n5 - Find integer\n6 - Remove integer\n7 - Reorder\n")
+		"4 - List all integers\n5 - Find integer\n6 - Remove integer\n7 - Sort\n")
 }
 
 func PrintGoodbyeMessage() {
@@ -57,4 +57,8 @@ func PrintRemoveValueIfExists() {
 
 func PrintInexistentValueMessage() {
 	fmt.Println("Inexistent value")
+}
+
+func PrintSortMessage() {
+	fmt.Println("1 - Sort ascending\n2 - Sort Descending")
 }
